47e09a9f8189bb00d976302d77ec08486febf523
moon maria <tatujjang@gmail.com>
2022-01-15 01:38:24 +0900

    File 생성자 테스트

--- src/test/java/live/munjeong/server/app/file/FileTest.java
+++ src/test/java/live/munjeong/server/app/file/FileTest.java
@@ -4,16 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FileTest {
     @Test
     @DisplayName("UploadFile 객체로 생성자 테스트")
     void constructorTestByUploadFile() {
-        UpLoadFile upLoadFile = new UpLoadFile("testFile1.JPg",540L);
+        UploadFile upLoadFile = new UploadFile("testFile1.JPg",540L);
         File file = new File(upLoadFile);
         Assertions.assertThat(file.getFileType()).isEqualTo(FileType.IMAGE);
-        Assertions.assertThat(file.getExtensions()).isEqualTo(FileType.IMAGE);
+        Assertions.assertThat(file.getExtensions()).isEqualTo("jpg");
     }
 
 }

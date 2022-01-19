d7d061e00ebdec5482cf2958aac7b7b94a43c2a0
kha0213 <tatujjang@gmail.com>
2022-01-19 13:53:44 +0900

    에러 핸들러 구현, JPA 자동 createBy, createDate 재 구현

---
+++ src/main/java/live/munjeong/server/api/common/ApiException.java
@@ -0,0 +1,13 @@
+package live.munjeong.server.api.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@RequiredArgsConstructor
+public class ApiException extends Exception {
+    private final ErrorCode errorCode;
+
+}
